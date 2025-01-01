254a78cf29a489bf6c5c78292b1e7137c090a263
precidennt2009 <precidennt2009@hotmail.com>
2025-01-01 17:54:15 -0500

    Cambio mensaje login adicion de otra linea

--- segver/segver/templates/login/index.php
+++ segver/segver/templates/login/index.php
@@ -97,9 +97,7 @@
             <div class="col-lg-6 d-flex align-items-center">
               <div class="text-black px-3 py-4 p-md-5 mx-md-4">
                 <h4 class="mb-4">Seguimiento y verificacion</h4>
-                <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p class="small mb-0">Texto de prueba para el login de la aplicacion de seguimiento y verificacion. Texto de prueba para el login de la aplicacion de seguimiento y verificacion.</p>
               </div>
 
             </div>
